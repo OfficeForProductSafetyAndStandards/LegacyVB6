2d04c45fca6e8b230419cecc76dc435e7b3279d1
Jiri Pinkava <j-pi@seznam.cz>
2011-05-27 02:52:28 +0200

    Prinv values of all defined modbus registers

--- eurotherm/qmodbuswidget.cpp
+++ eurotherm/qmodbuswidget.cpp
@@ -6,18 +6,26 @@ QModbusWidget::QModbusWidget(QWidget *parent, modbus_t *modbus, const ModbusReg 
 {
     QBoxLayout *layout;
     QWidget *w;
+    uint16_t val = 0;
+
+    if (modbusReg->read)
+        modbus_read_registers(modbus, modbusReg->regNum, 1, &val);
 
     if (!modbusReg->valNames) {
         w = spinBox_ = new QSpinBox(this);
         spinBox_->setRange(modbusReg_->valueMin, modbusReg_->valueMax);
         if (modbusReg_->specialValueName)
             spinBox_->setSpecialValueText(modbusReg_->specialValueName);
+        spinBox_->setValue(val);
     } else {
         const ValueName *valueName = modbusReg->valNames;
+        int idx;
 
         w = comboBox_ = new QComboBox(this);
-        for (int idx = 0; idx < modbusReg->namesCount; idx++)
-            comboBox_->addItem(valueName[idx].name, valueName[idx].value);
+        for (int x = 0; x < modbusReg->namesCount; x++)
+            comboBox_->addItem(valueName[x].name, valueName[x].value);
+        idx = comboBox_->findData(val);
+        comboBox_->setCurrentIndex(idx);
     }
 
     layout = new QBoxLayout(QBoxLayout::LeftToRight);
